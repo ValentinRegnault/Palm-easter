292e2af7072252bc8b720336590754264e81db8c
Pykorm <56791498+Pykorm@users.noreply.github.com>
2023-02-17 10:57:29 +0100

    Deploiement des questions quand on fait une requête a l'endpoint /deployQuestions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const { getDatabase } = require('firebase-admin/database');
+const {SECRET_CODE} = require("./secret-code.json") 
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
+admin.initializeApp()
+
+const db = getDatabase();
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+exports.deployQuestions = functions.https.onRequest((req, res) => {
+    if(!req.query.secretCode || req.query.secretCode != SECRET_CODE) {
+        res.status(403).send("Le code secret " + req.query.secretCode + " n'est pas valide :/")
+    }
+
+    functions.logger.info("Deploying questions to the database...")
+    const questions = require("./questions.json");  
+
+    db.ref("questions").set(questions).then(() => {
+        res.status(200).send("Questions loaded to database !")
+    })
+    .catch(() => {
+        res.status(403).send("An error occured :/");
+    })
+
+})
