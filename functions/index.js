48073b90804ea29144650034531054a56e5e1631
Pykorm <56791498+Pykorm@users.noreply.github.com>
2023-02-20 14:40:05 +0100

    Génération des questions
    Une cloud function qui ajoute les questions à un utilisateur quand il vient d'être ajouté, en les piochant aléatoirement parmis toute les questions.
    j'ai aussi viré l'endpoint getQuestions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-const { getDatabase } = require('firebase-admin/database');
-const {SECRET_CODE} = require("./secret-code.json") 
+const { getDatabase, ref, get, child } = require('firebase-admin/database');
+const {SECRET_CODE} = require("./secret-code.json"); 
+const { ObjectRendererSystem } = require("pixi.js");
 
 admin.initializeApp()
 
 const db = getDatabase();
+
+const QUESTION_MAX_LEVEL = 5
+const QUESTIONS_COUNT = 10
+
+const QUESTIONS_COUNT_BY_LEVEL = [2, 2, 2, 3, 1]
 
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -40,6 +46,27 @@ exports.deployQuestions = functions.https.onRequest((req, res) => {
     })
 })
 
-exports.getQuestions = functions.https.onRequest((req, res) => {
+exports.onUserCreated = functions.database.ref("/users/{userId}")
+    .onCreate(async (snapshot, context) => {
+        let questions = await ref.once("/questions")
 
-})
+        let choosenQuestions = []
+        for (let i = 0; i < QUESTIONS_COUNT_BY_LEVEL.length; i++) {
+            let questionsLevelI = questions.filter(q => q.level == i); 
+            let differentQuestion = 0;
+            while (differentQuestion < QUESTIONS_COUNT_BY_LEVEL[i]) {
+                let rdIndex = Math.floor(Math.random() * questionsLevelI.length)
+                if (!choosenQuestions.includes(rdIndex)) {
+                    choosenQuestions.push(rdIndex)
+                    differentQuestion++
+                }
+            }
+        }
+
+        let allQuestionsIds = Object.keys(questions)
+        let choosenQuestionsIds = choosenQuestions.map(index => allQuestionsIds[index])
+
+        snapshot.ref.update({
+            questions: choosenQuestionsIds
+        })
+    })
